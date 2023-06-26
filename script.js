d2d77db7fc79d4cea666d63d2183a691abfb3554
matheus-mmr <137805036+matheus-mmr@users.noreply.github.com>
2023-06-26 13:46:55 -0300

    projeto

---
+++ script.js
@@ -0,0 +1,16 @@
+    var typed = new Typed("#multiple-text", {
+    strings: ["Tutoriais", "Sobre Desenvolvimento Web", "Ferramentas"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+}) 
+
+ /* var trocat = 0;
+var txt = ["tutoriais", "oi", "text2", "txt3"];
+var spantext = document.querySelector("#text1");
+setInterval(() => {
+    trocat == 0 ? trocat = 1 : trocat =  0
+    spantext.innerHTML = txt[trocat]    
+
+}, 5000) */
