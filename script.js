ae35aca4b41b2158abd8cf26681fb50165e8985e
matheus-mmr <137805036+matheus-mmr@users.noreply.github.com>
2023-06-26 14:40:33 -0300

    animação

--- script.js
+++ script.js
@@ -14,3 +14,22 @@ setInterval(() => {
     spantext.innerHTML = txt[trocat]    
 
 }, 5000) */
+
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+    var reveals = document.querySelectorAll('.reveal');
+    var windowheight = window.innerHeight;
+    var revealtop;
+    var revealpoint = 150;
+
+    for (var i = 0; i < reveals.length; i++) {
+        revealtop = reveals[i].getBoundingClientRect().top;
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add('active');
+        } else {
+            reveals[i].classList.remove('active');
+        }
+    }
+
+}
